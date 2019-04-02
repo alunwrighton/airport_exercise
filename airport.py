797e1026160194fd9b2a6b3d105b4f3800d9feaf
Alun Wrighton <AWrighton@spartaglobal.com>
2019-04-02 17:06:43 +0100

    Aluns updated work

--- airport.py
+++ airport.py
@@ -1,3 +1,28 @@
-class Airport
+# Creating Airport class
 
-    def __init__(self, name, location, ):
+class Airport:
+
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        self.terminals = []
+
+# Creating Terminal class
+
+class Terminal:
+
+    def __init__(self, name, location, airport):
+        self.name = name
+        self.location = location
+        self.airport = airport
+        self.flights = []
+
+    def add_flights(self, flight):
+        self.flights.append(flight)
+
+heathrow = Airport("Heathrow", "London")
+
+terminal1 = Terminal("T1", "North", heathrow)
+terminal1.add_flights("FG145")
+print(terminal1.flights)
+
---
+++ passenger.py
@@ -0,0 +1,16 @@
+class Passenger:
+    def __init__(self, name, flight, age)
+        self.name = name
+        self.flight = flight
+        self.age = age
+
+    def add_to_flight(self, flight):
+        self.flight = flight
+        flight.add_passenger.append(self)
+
+
+
+
+
+
+
